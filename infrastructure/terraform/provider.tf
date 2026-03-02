af6ec4f6dfe078af8eeb4ff5774b104967101469
moushmigrace <moushmigrace@gmail.com>
2026-03-02 17:24:06 +0530

    changes added

--- infrastructure/terraform/provider.tf
+++ infrastructure/terraform/provider.tf
@@ -15,3 +15,13 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.eks.token
 }
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
--- infrastructure/terraform/vpc.tf
+++ infrastructure/terraform/vpc.tf
@@ -1,3 +1,7 @@
+############################
+# VPC
+############################
+
 resource "aws_vpc" "finops_vpc" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
@@ -8,22 +12,83 @@ resource "aws_vpc" "finops_vpc" {
   }
 }
 
-resource "aws_subnet" "subnet1" {
-  vpc_id            = aws_vpc.finops_vpc.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-east-1a"
+############################
+# Internet Gateway
+############################
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.finops_vpc.id
 
   tags = {
-    Name = "finops-subnet-1"
+    Name = "finops-igw"
   }
 }
 
+############################
+# Public Subnet 1
+############################
+
+resource "aws_subnet" "subnet1" {
+  vpc_id                  = aws_vpc.finops_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "finops-subnet-1"
+
+    # REQUIRED FOR EKS LOADBALANCER
+    "kubernetes.io/cluster/finops-cluster" = "shared"
+    "kubernetes.io/role/elb"               = "1"
+  }
+}
+
+############################
+# Public Subnet 2
+############################
+
 resource "aws_subnet" "subnet2" {
-  vpc_id            = aws_vpc.finops_vpc.id
-  cidr_block        = "10.0.2.0/24"
-  availability_zone = "us-east-1b"
+  vpc_id                  = aws_vpc.finops_vpc.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "finops-subnet-2"
+
+    # REQUIRED FOR EKS LOADBALANCER
+    "kubernetes.io/cluster/finops-cluster" = "shared"
+    "kubernetes.io/role/elb"               = "1"
   }
+}
+
+############################
+# Route Table
+############################
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.finops_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "finops-public-rt"
+  }
+}
+
+############################
+# Route Table Associations
+############################
+
+resource "aws_route_table_association" "subnet1_assoc" {
+  subnet_id      = aws_subnet.subnet1.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "subnet2_assoc" {
+  subnet_id      = aws_subnet.subnet2.id
+  route_table_id = aws_route_table.public_rt.id
 }
