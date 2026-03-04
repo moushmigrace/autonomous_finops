29ebe589cd0658003087f29c0b8ce5bda37fb075
moushmigrace <moushmigrace@gmail.com>
2026-03-04 12:42:05 +0530

    fix: use GHCR images

--- infrastructure/terraform/nodegroup.tf
+++ infrastructure/terraform/nodegroup.tf
@@ -14,7 +14,7 @@ resource "aws_eks_node_group" "node_group" {
     min_size     = 1
   }
 
-  instance_types = ["t3.medium"]
+  instance_types = ["t3.micro"]
 
   depends_on = [
     aws_iam_role_policy_attachment.worker_node_policy,
